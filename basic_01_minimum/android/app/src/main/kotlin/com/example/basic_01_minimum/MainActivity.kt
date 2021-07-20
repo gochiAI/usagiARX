5c8776e9807426444d0a4e2a8870e191ba2294f7
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-07-20 13:32:30 +0900

    Add basic_01_minimum example

---
+++ basic_01_minimum/android/app/src/main/kotlin/com/example/basic_01_minimum/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.basic_01_minimum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ basic_01_minimum/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'Basic-01 Minimum',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: Center(
+          child: Text('Basic-01 Minimum'),
+        ),
+      )));
+}
