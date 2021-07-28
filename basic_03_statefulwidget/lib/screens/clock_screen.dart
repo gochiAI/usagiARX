02b04cabf1108dcd86d16d24910e2a7eaf1173b3
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-07-29 01:47:58 +0900

    Update

--- basic_03_statefulwidget/lib/screens/clock_screen.dart
+++ basic_03_statefulwidget/lib/screens/clock_screen.dart
@@ -65,14 +65,9 @@ class _ClockScreenState extends State<ClockScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            _formatter.format(dt),
-            style: Theme.of(context).textTheme.headline2,
-          ),
-        ],
+          child: Text(
+        _formatter.format(dt),
+        style: Theme.of(context).textTheme.headline2,
       )),
     );
   }
