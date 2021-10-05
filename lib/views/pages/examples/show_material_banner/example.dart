9f32ca038ccfa8218e4c19d5f6dbae4f029e4999
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-10-05 18:27:54 +0900

    Update ShowMaterialBanner example

--- lib/views/pages/examples/show_material_banner/example.dart
+++ lib/views/pages/examples/show_material_banner/example.dart
@@ -16,29 +16,81 @@ class _State extends State<ShowMaterialBanner> {
         title: const Text('Show Material Banner'),
         centerTitle: true,
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('show'),
-          onPressed: () {
-            final messenger = ScaffoldMessenger.of(context);
+      body: _container(
+        children: [
+          _sized(
+            _button(
+              'show Material Banner',
+              _showMaterialBanner,
+            ),
+          ),
+          _spacer(),
+          _sized(
+            _button(
+              'show Snack Bar',
+              _showSnackBar,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-            messenger.showMaterialBanner(
-              MaterialBanner(
-                content: const Text('hello'),
-                leading: const Icon(Icons.info),
-                backgroundColor: Colors.yellow,
-                actions: [
-                  TextButton(
-                    child: const Text('dismiss'),
-                    onPressed: () {
-                      messenger.hideCurrentMaterialBanner();
-                    },
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
+  Widget _container({List<Widget> children = const <Widget>[]}) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: children,
+      ),
+    );
+  }
+
+  Widget _sized(Widget w) {
+    return SizedBox(
+      child: w,
+      width: 200,
+    );
+  }
+
+  Widget _spacer() {
+    return const SizedBox(
+      height: 15,
+    );
+  }
+
+  Widget _button(String text, Function() fn) {
+    return ElevatedButton(
+      child: Text(text),
+      onPressed: fn,
+    );
+  }
+
+  void _showMaterialBanner() {
+    final messenger = ScaffoldMessenger.of(context);
+
+    messenger.showMaterialBanner(
+      MaterialBanner(
+        content: const Text('これが Material Banner です.'),
+        leading: const Icon(Icons.info),
+        backgroundColor: Colors.amber,
+        actions: [
+          TextButton(
+            child: const Text('消す'),
+            onPressed: () {
+              messenger.hideCurrentMaterialBanner();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showSnackBar() {
+    final messenger = ScaffoldMessenger.of(context);
+
+    messenger.showSnackBar(
+      const SnackBar(
+        content: Text('これが Snack Bar です'),
       ),
     );
   }
